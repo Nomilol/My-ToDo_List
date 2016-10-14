d096025391854631b1fc9c57cafff56c5610e38a
Axel <lezinaxel@gmail.com>
2016-10-14 08:51:22 +0200

    Ajout du bouton ' valider tâches '

---
+++ app.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+
+$('.add').on('click', function(){
+	//console.log('coucou');
+	var add = $('input[name=list').val();
+	$('ul').append("<li>" + add + "</li>"); 
+})
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
